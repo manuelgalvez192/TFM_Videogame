0d2dde7181290b49d9b655e03e368a650f9dc1c1
Manuel <manuelgalvez192@gmail.com>
2023-07-27 18:08:09 +0200

    camera follow and limit left map

--- EscocesPeregrino/Assets/Scripts/CamerFollow.cs
+++ EscocesPeregrino/Assets/Scripts/CamerFollow.cs
@@ -1,8 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CamerFollow : MonoBehaviour
 {
-    
+    [SerializeField]
+    private Transform player;
+    public Vector3 offset;
+
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = player.position + offset;
+
+        // Solo actualizamos la posición en el eje X, manteniendo el valor actual en el eje Y
+        desiredPosition.y = transform.position.y;
+        desiredPosition.z = transform.position.z;
+
+        // Actualizamos directamente la posición de la cámara
+        transform.position = desiredPosition;
+    }
 }
