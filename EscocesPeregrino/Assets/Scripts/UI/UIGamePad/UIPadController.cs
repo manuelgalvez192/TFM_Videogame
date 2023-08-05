16cfb68743435be2a0a458ac5c1eb06fd79b32d9
DaniFort <danifortvideogamedeveloper@gmail.com>
2023-08-05 20:30:29 +0200

    botnones creados
    Vamoh a ver si ahora no se rompe con el inputSystem

--- EscocesPeregrino/Assets/Scripts/UI/UIGamePad/UIPadController.cs
+++ EscocesPeregrino/Assets/Scripts/UI/UIGamePad/UIPadController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 
@@ -11,18 +12,36 @@ public class UIPadController : MonoBehaviour, IPointerDownHandler, IPointerUpHan
 
 
     //Funcionality
-    bool isPadActive = false;
+    bool isJoystickActive = false;
+    bool isbuttonsPadActive = false;
 
-    [SerializeField] float horizontalAxis;
-    [SerializeField] float verticalAxis;
+    float horizontalAxis = 0;
+    float verticalAxis =0 ;
 
-    //gettersSetters
-    public bool IsPadActive
+    [SerializeField] UIPadButton jumpButton;
+    [SerializeField] UIPadButton attackButton;
+    [SerializeField] UIPadButton coverButton;
+    [SerializeField] UIPadButton dashButton;
+    private void Awake()
     {
-        get { return isPadActive; }
+        jumpButton.ControllerParent = this;
+        attackButton.ControllerParent = this;
+        coverButton.ControllerParent = this;
+        dashButton.ControllerParent = this;
     }
 
+    //gettersSetters
+    public bool IsJoystickActive
+    {
+        get { return isJoystickActive; }
+    }
+    public bool IsButtonsPadActive
+    {
+        set { isbuttonsPadActive = value; }
+        get { return isJoystickActive; }
+    }
 
+    #region Joystick
     public float HorizontalAxis
     {
         get { return horizontalAxis; }
@@ -36,7 +55,7 @@ public class UIPadController : MonoBehaviour, IPointerDownHandler, IPointerUpHan
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        isPadActive = true;
+        isJoystickActive = true;
 
         //jpystick things
         joyStick.SetActive(true);
@@ -45,13 +64,68 @@ public class UIPadController : MonoBehaviour, IPointerDownHandler, IPointerUpHan
     }
     public void OnPointerUp(PointerEventData eventData)
     {
-        isPadActive = false;
+        isJoystickActive = false;
         joyStick.SetActive(false);
     }
-
     void ResetValues()//Aqui reseteamops todos los valores
     {
         horizontalAxis = 0;
         verticalAxis = 0;
     }
+    #endregion
+    #region Buttons
+    //down
+    public bool JumpButtonDown
+    {
+        get { return jumpButton.GetPadButtonDown(); }
+    }
+    public bool CoverButtonDown
+    {
+        get { return coverButton.GetPadButtonDown(); }
+    }
+    public bool DashButtonDown
+    {
+        get { return dashButton.GetPadButtonDown(); }
+    }
+    public bool AttackButtonDown
+    {
+        get { return attackButton.GetPadButtonDown(); }
+    }
+    //pressed
+    public bool JumpButton
+    {
+        get { return jumpButton.GetPadButton(); }
+    }
+    public bool CoverButton
+    {
+        get { return coverButton.GetPadButton(); }
+    }
+    public bool DashButton
+    {
+        get { return dashButton.GetPadButton(); }
+    }
+    public bool AttackButton
+    {
+        get { return attackButton.GetPadButton(); }
+    }
+    //Up
+    public bool JumpButtonUp
+    {
+        get { return jumpButton.GetPadButtonUp(); }
+    }
+    public bool CoverButtonUp
+    {
+        get { return coverButton.GetPadButtonUp(); }
+    }
+    public bool DashButtonUp
+    {
+        get { return dashButton.GetPadButtonUp(); }
+    }
+    public bool AttackButtonUp
+    {
+        get { return attackButton.GetPadButtonUp(); }
+    }
+    #endregion
+
 }
+
