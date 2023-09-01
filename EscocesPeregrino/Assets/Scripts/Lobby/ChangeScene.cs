45b9c4daaab7aa803c42a49216b4d14d4be3b60a
Raulmind <rmingallon@gmail.com>
2023-09-01 16:09:48 +0200

    Added script to change scene, and lobby scene

---
+++ EscocesPeregrino/Assets/Scripts/Lobby/ChangeScene.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    [SerializeField] private string changeLevel;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(changeLevel);
+        }
+    }
+}
