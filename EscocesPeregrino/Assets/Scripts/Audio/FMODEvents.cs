905f36dab3e65445dc0a2e57fcebce5bacb629d9
Raulmind <rmingallon@gmail.com>
2023-09-02 20:20:25 +0200

    Same that before merge

---
+++ EscocesPeregrino/Assets/Scripts/Audio/FMODEvents.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class FMODEvents : MonoBehaviour
+{
+    [field: Header("Player SFX")]
+    [field: SerializeField] public EventReference playerFootsteps { get; private set; }
+    [field: SerializeField] public EventReference playerHit { get; private set; }
+
+    [field: Header ("Menu SFX")]
+    [field: SerializeField] public EventReference menuClick { get; private set; }
+    public static FMODEvents instance { get; private set; }
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.LogError("FOund more than one FMOD Events instance in the scene");
+        }
+        instance= this;
+    }
+}
--- EscocesPeregrino/Assets/Scripts/MainMenu/MainMenuManager.cs
+++ EscocesPeregrino/Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -2,17 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class MainMenuManager : MonoBehaviour
 {
+    
     void Start()
     {
     }
     public void PlayGame()
     {
+        AudioManager.instance.PlayOneShot(FMODEvents.instance.menuClick, this.transform.position);
         UnityEngine.SceneManagement.SceneManager.LoadScene(1);
     }
     public void Exit()
     {
         Application.Quit();
+    }
+
+    public void DoSound()
+    {
+        AudioManager.instance.PlayOneShot(FMODEvents.instance.menuClick, this.transform.position);
     }
 }
