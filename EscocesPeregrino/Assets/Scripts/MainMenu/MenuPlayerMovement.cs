f13425c8eb81d957529bd59e7515d3c620cc5fbd
DaniFort <danifortvideogamedeveloper@gmail.com>
2023-09-05 12:59:41 +0200

    EscenaJugador

--- EscocesPeregrino/Assets/Scripts/MainMenu/MenuPlayerMovement.cs
+++ EscocesPeregrino/Assets/Scripts/MainMenu/MenuPlayerMovement.cs
@@ -12,14 +12,28 @@ public class MenuPlayerMovement : MonoBehaviour
     [SerializeField] float speedY;
     [SerializeField] float speedZ;
     [SerializeField] Animator anim;
+    [SerializeField] Transform render;
+
+    [Header("Jump")]
+    [SerializeField, Range(0.1f, 1)] private float jumpPower = 0.35f;
+    [SerializeField] AnimationCurve jumpCurve;
+    bool isGrounded =true;
+    [Header("Block")]
+    bool isBlocking;
+
+    [Header("Particle System")]
+    [SerializeField] ParticleSystem dustMovementPS;
+    [SerializeField] ParticleSystem dustJumpPS;
+    float movementDustCount;
+    [SerializeField, Range(0, 0.1f)] float movementDustRate = 0.08f;
+
     void Start()
     {
-        
+        anim.SetBool("isGrounded", true);
     }
 
     void Update()
     {
-
         Movement();
         CheckLimits();
         SetAnimatorValues();
@@ -49,26 +63,88 @@ public class MenuPlayerMovement : MonoBehaviour
     }
     void SetAnimatorValues()
     {
+        if (InputsGameManager.instance.CoverButtonDown)
+        {
+            isBlocking = true;
+            anim.SetBool("Cover", true);
+        }
+            if (InputsGameManager.instance.CoverButtonUp)
+        {
+            isBlocking = false;
+            anim.SetBool("Cover", false);
+        }
+        if (isBlocking)
+            return;
         if (InputsGameManager.instance.HorizontalAxis != 0 || InputsGameManager.instance.VerticalAxis != 0)
+        {
             anim.SetBool("isRunning", true);
+            ThrowMovementDust();
+        }
         else
+        {
             anim.SetBool("isRunning", false);
+            StopMovementDust();
+        }
 
         if(InputsGameManager.instance.AttackButtonDown)
         {
             anim.SetTrigger("Attack");
+            StartCoroutine(AfterAttack(0.3f));
         }
-        if (InputsGameManager.instance.JumpButton)
+        if (InputsGameManager.instance.JumpButtonDown)
         {
-            anim.SetTrigger("Jump");
+            Jump();
         }
-        if (InputsGameManager.instance.CoverButtonUp)
+        if(InputsGameManager.instance.DashButtonDown)
         {
-            print("Cover");
+            anim.SetTrigger("SpecialAttack");
+            StartCoroutine(AfterAttack(0.7f));
+            
         }
+    }
+
+    private void Jump()
+    {
+        if (!isBlocking)
+        {
+            if (isGrounded)
+            {
+                isGrounded = false;
+
+                StartCoroutine(JumpBehaviour());
+            }
+        }
+    }
+
+    IEnumerator JumpBehaviour()
+    {
+        isGrounded = false;
+        float currentHeight = 0;
+        float maxTime = jumpCurve[jumpCurve.length - 1].time;
+        float elapsedTime = 0;
+        anim.SetBool("isGrounded", false);
+        ThrowJumpDust();
+
+
+        while (elapsedTime <= maxTime)
+        {
+            currentHeight = jumpCurve.Evaluate(elapsedTime) * jumpPower;
+            render.localPosition = new Vector2(render.localPosition.x, currentHeight);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        isGrounded = true;
+        anim.SetBool("isGrounded", true);
+        render.localPosition = Vector2.zero;
+        ThrowJumpDust();
+
+        yield break;
     }
     void Movement()
     {
+        if (isBlocking)
+            return;
         transform.Translate(transform.right * InputsGameManager.instance.HorizontalAxis * speedX * Time.deltaTime);
         transform.Translate(transform.up * InputsGameManager.instance.VerticalAxis * speedY * Time.deltaTime);
 
@@ -83,6 +159,24 @@ public class MenuPlayerMovement : MonoBehaviour
             transform.rotation = newRot;
         }
     }
-
+    IEnumerator AfterAttack(float time)
+    {
+        isBlocking = true;
+        yield return new WaitForSeconds(time);
+        isBlocking = false;
+        yield break;
+    }
+    void ThrowMovementDust()
+    {
+        dustMovementPS.Play();
+    }
+    void StopMovementDust()
+    {
+        dustMovementPS.Stop();
+    }
+    void ThrowJumpDust()
+    {
+        dustJumpPS.Play();
+    }
 
 }
