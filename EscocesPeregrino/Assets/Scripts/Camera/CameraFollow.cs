1e68ca0d3d589fe3540f85aaa203a2b51a1e3d36
Manuel <manuelgalvez192@gmail.com>
2023-09-05 22:52:51 +0200

    fixed camera follow, and enemy collision

---
+++ EscocesPeregrino/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private float minPlayerPosition;
+    [SerializeField] private float maxPlayerPosition;
+    
+    public Vector3 offset;
+
+    void FixedUpdate()
+    {
+        if (player.position.x > minPlayerPosition && player.position.x < maxPlayerPosition)
+        {
+            Vector3 desiredPosition = player.position + offset;
+
+            // Solo actualizamos la posición en el eje X, manteniendo el valor actual en el eje Y
+            desiredPosition.y = transform.position.y;
+            desiredPosition.z = transform.position.z;
+
+            // Actualizamos directamente la posición de la cámara
+            transform.position = desiredPosition;
+        }
+    }
+}
